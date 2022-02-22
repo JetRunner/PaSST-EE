1910690d347d445ebde295391c1babd948916327
actuy <841223401@qq.com>
2022-02-23 00:00:33 +0800

    add resume ckpt for running scripts

--- runs/passt_ee_base.sh
+++ runs/passt_ee_base.sh
@@ -1,9 +1,22 @@
 ngpu=${ngpu:-2}
 cuda_devices=${cuda_devices:-0,1}
 exp_name=${exp_name:-base}
+resume_from=${resume_from:-null}
 
 if [[ $ngpu -eq 1 ]]; then
-    python ex_audioset.py with trainer.precision=16 trainer.default_root_dir="output/PaSST-EE-${exp_name}-1gpu" models.net.arch=passt_deit_bd_p16_384 -p -F "output/PaSST-EE-${exp_name}-1gpu/audioset/logs" -c "PaSST EE ${exp_name}"
+    python ex_audioset.py \
+    with trainer.precision=16 \
+        trainer.resume_from_checkpoint=${resume_from} \
+        trainer.default_root_dir="output/PaSST-EE-${exp_name}-1gpu" \
+        models.net.arch=passt_deit_bd_p16_384 \
+        -p -F "output/PaSST-EE-${exp_name}-1gpu/audioset/logs" \
+        -c "PaSST EE ${exp_name}"
 else
-    CUDA_VISIBLE_DEVICES=$cuda_devices DDP=${ngpu} python ex_audioset.py with trainer.precision=16 trainer.default_root_dir="output/PaSST-EE-${exp_name}-${ngpu}gpu" models.net.arch=passt_deit_bd_p16_384 -p -F "output/PaSST-EE-${exp_name}-${ngpu}gpu/audioset/logs" -c "PaSST ${exp_name} ${ngpu} GPU"
+    CUDA_VISIBLE_DEVICES=$cuda_devices DDP=${ngpu} python ex_audioset.py \
+    with trainer.precision=16 \
+        trainer.resume_from_checkpoint=${resume_from} \
+        trainer.default_root_dir="output/PaSST-EE-${exp_name}-${ngpu}gpu" \
+        models.net.arch=passt_deit_bd_p16_384 \
+        -p -F "output/PaSST-EE-${exp_name}-${ngpu}gpu/audioset/logs" \
+        -c "PaSST ${exp_name} ${ngpu} GPU"
 fi
--- runs/passt_ee_distil.sh
+++ runs/passt_ee_distil.sh
@@ -1,10 +1,25 @@
 ngpu=${ngpu:-2}
 cuda_devices=${cuda_devices:-0,1}
 exp_name=${exp_name:-distil}
+resume_from=${resume_from:-null}
 distillation_alpha=${distillation_alpha:-0.9}
 
 if [[ $ngpu -eq 1 ]]; then
-    python ex_audioset_oracle_distillation.py with trainer.precision=16 distillation_alpha=${distillation_alpha} trainer.default_root_dir="output/PaSST-EE-${exp_name}-1gpu" models.net.arch=passt_deit_bd_p16_384 -p -F "output/PaSST-EE-${exp_name}-1gpu/audioset/logs" -c "PaSST EE ${exp_name}"
+    python ex_audioset_oracle_distillation.py \
+    with trainer.precision=16 \
+        trainer.resume_from_checkpoint=${resume_from} \
+        distillation_alpha=${distillation_alpha} \
+        trainer.default_root_dir="output/PaSST-EE-${exp_name}-1gpu" \
+        models.net.arch=passt_deit_bd_p16_384 \
+        -p -F "output/PaSST-EE-${exp_name}-1gpu/audioset/logs" \
+        -c "PaSST EE ${exp_name}"
 else
-    CUDA_VISIBLE_DEVICES=$cuda_devices DDP=${ngpu} python ex_audioset_oracle_distillation.py with trainer.precision=16 distillation_alpha=${distillation_alpha} trainer.default_root_dir="output/PaSST-EE-${exp_name}-${ngpu}gpu" models.net.arch=passt_deit_bd_p16_384 -p -F "output/PaSST-EE-${exp_name}-${ngpu}gpu/audioset/logs" -c "PaSST ${exp_name} ${ngpu} GPU"
+    CUDA_VISIBLE_DEVICES=$cuda_devices DDP=${ngpu} python ex_audioset_oracle_distillation.py \
+    with trainer.precision=16 \
+        trainer.resume_from_checkpoint=${resume_from} \
+        distillation_alpha=${distillation_alpha} \
+        trainer.default_root_dir="output/PaSST-EE-${exp_name}-${ngpu}gpu" \
+        models.net.arch=passt_deit_bd_p16_384 \
+        -p -F "output/PaSST-EE-${exp_name}-${ngpu}gpu/audioset/logs" \
+        -c "PaSST ${exp_name} ${ngpu} GPU"
 fi
